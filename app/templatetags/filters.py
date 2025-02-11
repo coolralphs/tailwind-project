2f0988655b82793c86597a9397cacbd4019ed6cd
Rafael Salazar <coolralph_s@hotmail.com>
2025-02-11 02:34:20 -0600

    colored markers; horizontal date scrollbar;

--- app/templatetags/filters.py
+++ app/templatetags/filters.py
@@ -1,4 +1,5 @@
 from django import template
+from django_countries import countries
 
 register = template.Library()
 
@@ -24,3 +25,30 @@ def dictsorttwo(value, key):
 @register.filter
 def get_fields(obj):
     return [(field.name, field.value_to_string(obj)) for field in obj._meta.fields]
+
+@register.filter
+def unique_countries(queryset, field_name):
+    seen = set()
+    result = []
+
+    # Create a lookup dictionary with lowercased country names and codes
+    country_dict = {name.lower(): code for code, name in countries}
+    
+    for obj in queryset:
+        field_value = getattr(obj, field_name, "").strip().lower()  # Handle empty values
+
+        # Try exact match first
+        if field_value == "united states":
+            country_code = "US"
+        elif field_value in country_dict:
+            country_code = country_dict[field_value]
+        else:
+            # If no match is found, return the full country name
+            country_code = field_value.title()  # Capitalize first letter of the country name
+
+        # Add the country code or full name to the result if it's not already in the set
+        if country_code not in seen:
+            seen.add(country_code)
+            result.append(country_code)
+
+    return result
